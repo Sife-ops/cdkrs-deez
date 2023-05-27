742ad2b3e176aa3ce89c7a883286d26f0f662aa2
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-05-27 07:28:21 -0700

    table

--- lib/cdkrs-stack.ts
+++ lib/cdkrs-stack.ts
@@ -1,6 +1,7 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { RustFunction } from "cargo-lambda-cdk";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import {
   CorsHttpMethod,
   HttpApi,
@@ -12,7 +13,29 @@ export class CdkrsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // https://github.com/bobbyhadz/aws-cdk-http-api-apigateway-v2-example/blob/cdk-v2/lib/cdk-starter-stack.ts
+    const table = new dynamodb.Table(this, id, {
+      tableName: "cdkrs-table",
+      billingMode: dynamodb.BillingMode.PROVISIONED,
+      readCapacity: 1,
+      writeCapacity: 1,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
+      sortKey: { name: "sk", type: dynamodb.AttributeType.STRING },
+      pointInTimeRecovery: true,
+    });
+
+    // table.addLocalSecondaryIndex({
+    //   indexName: 'statusIndex',
+    //   sortKey: {name: 'status', type: dynamodb.AttributeType.STRING},
+    //   projectionType: dynamodb.ProjectionType.ALL,
+    // });
+
+    // todo: table.addGlobalSecondaryIndex
+
+    /**
+     * example: https://github.com/bobbyhadz/aws-cdk-http-api-apigateway-v2-example/blob/cdk-v2/lib/cdk-starter-stack.ts
+     * graduation: https://github.com/aws/aws-cdk/discussions/24038
+     */
     const api = new HttpApi(this, "cdkrs-api", {
       corsPreflight: {
         allowHeaders: [
@@ -39,7 +62,14 @@ export class CdkrsStack extends cdk.Stack {
     const hwl = new RustFunction(this, "hw", {
       manifestPath: "functions/hw/Cargo.toml",
       // todo: https://github.com/cargo-lambda/cargo-lambda-cdk#rust-extension
+      bundling: {
+        environment: {
+          TABLE_NAME: table.tableName,
+        },
+      },
     });
+
+    table.grantFullAccess(hwl);
 
     api.addRoutes({
       path: "/hw",
