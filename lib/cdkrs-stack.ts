7259b0a15f8ec9b62f688f40937f2622649eec8c
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-05-27 10:01:40 -0700

    reconfigure table

--- lib/cdkrs-stack.ts
+++ lib/cdkrs-stack.ts
@@ -1,23 +1,24 @@
-import * as cdk from "aws-cdk-lib";
-import { Construct } from "constructs";
-import { RustFunction } from "cargo-lambda-cdk";
-import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
 import {
   CorsHttpMethod,
   HttpApi,
   HttpMethod,
 } from "@aws-cdk/aws-apigatewayv2-alpha";
 import { HttpLambdaIntegration } from "@aws-cdk/aws-apigatewayv2-integrations-alpha";
+import * as cdk from "aws-cdk-lib";
+import * as dynamodb from "aws-cdk-lib/aws-dynamodb";
+import { RustFunction } from "cargo-lambda-cdk";
+import { Construct } from "constructs";
 
 export class CdkrsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    /**
+     * https://github.com/bobbyhadz/aws-cdk-dynamodb-table/blob/cdk-v2/lib/cdk-starter-stack.ts
+     */
     const table = new dynamodb.Table(this, id, {
       tableName: "cdkrs-table",
-      billingMode: dynamodb.BillingMode.PROVISIONED,
-      readCapacity: 1,
-      writeCapacity: 1,
+      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       partitionKey: { name: "pk", type: dynamodb.AttributeType.STRING },
       sortKey: { name: "sk", type: dynamodb.AttributeType.STRING },
