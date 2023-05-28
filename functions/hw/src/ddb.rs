7c52c925a01527fb5d1a54bcaa925e57915bfbee
Wyatt Goettsch <wyatt.goettsch@gmail.com>
2023-05-28 11:39:11 -0700

    maplit

--- functions/hw/src/ddb.rs
+++ functions/hw/src/ddb.rs
@@ -3,49 +3,57 @@ use aws_sdk_dynamodb::types::AttributeValue;
 use aws_sdk_dynamodb::Client;
 use std::collections::HashMap;
 
-#[derive(Eq, PartialEq, Hash)]
+// todo: expose
+#[derive(Eq, PartialEq, Hash, Debug)]
 pub enum IndexName {
     Primary,
     Gsi1,
     Gsi2,
 }
 
+#[derive(Debug)]
 pub struct Composite {
     pub name: String,
     pub value: String,
 }
 
+#[derive(Debug)]
 pub struct Key {
     pub field: String,
     pub composite: Vec<Composite>,
 }
 
+impl Key {
+    fn _join_composite(&self) -> String {
+        let mut s = String::new();
+        for composite in self.composite.iter() {
+            s.push_str("#");
+            s.push_str(&composite.name);
+            s.push_str("_");
+            s.push_str(&composite.value);
+        }
+        s
+    }
+}
+
+#[derive(Debug)]
 pub struct Index {
     pub partition_key: Key,
     pub sort_key: Key,
 }
 
 pub enum Attribute {
-    DdbString(String),
-    DdbBoolean(bool),
-    DdbNumber(i64),
+    DdbString(Option<String>),
+    DdbBoolean(Option<bool>),
+    DdbNumber(Option<i64>),
 }
 
 pub struct EntitySchema {
+    pub table: String,
     pub service: String,
     pub entity: String,
     pub indices: HashMap<IndexName, Index>,
-    pub attributes: HashMap<String, Attribute>, // todo: generic value
-}
-
-fn key_to_av(s: &mut String, k: &Key) -> AttributeValue {
-    for composite in k.composite.iter() {
-        s.push_str("#");
-        s.push_str(&composite.name);
-        s.push_str("_");
-        s.push_str(&composite.value);
-    }
-    AttributeValue::S(s.to_string())
+    pub attributes: HashMap<String, Attribute>,
 }
 
 pub trait DdbEntity {
@@ -56,30 +64,30 @@ pub trait DdbEntity {
         let mut m = HashMap::new();
         m.insert(
             "_entity".to_string(),
-            AttributeValue::S(entity_schema.entity),
+            AttributeValue::S(entity_schema.entity.clone()),
         );
         // indexes
         for (_, index) in &entity_schema.indices {
             // partition key
-            let mut pk = String::from("$"); // todo: not dry
+            let mut pk = String::from("$");
             pk.push_str(&entity_schema.service);
-            m.insert(
-                index.partition_key.field.clone(),
-                key_to_av(&mut pk, &index.partition_key),
-            );
+            pk.push_str("#");
+            pk.push_str(&entity_schema.entity);
+            pk.push_str(&index.partition_key._join_composite());
+            m.insert(index.partition_key.field.clone(), AttributeValue::S(pk));
             // sort key
             let mut sk = String::from("$");
-            sk.push_str(&entity_schema.service);
-            m.insert(
-                index.sort_key.field.clone(),
-                key_to_av(&mut sk, &index.sort_key),
-            );
+            sk.push_str(&entity_schema.entity);
+            sk.push_str(&index.sort_key._join_composite());
+            m.insert(index.sort_key.field.clone(), AttributeValue::S(sk));
         }
         // attributes
         for (k, v) in &entity_schema.attributes {
             match v {
-                Attribute::DdbString(a) => {
-                    m.insert(k.to_string(), AttributeValue::S(a.to_string()));
+                Attribute::DdbString(o) => {
+                    if let Some(s) = o {
+                        m.insert(k.to_string(), AttributeValue::S(s.to_string()));
+                    }
                 }
                 Attribute::DdbBoolean(_) => {} // todo
                 Attribute::DdbNumber(_) => {}  // todo
@@ -89,7 +97,7 @@ pub trait DdbEntity {
     }
 
     fn put(&self, c: &Client) -> PutItemFluentBuilder {
-        let mut req = c.put_item().table_name("cdkrs-table"); // todo: global
+        let mut req = c.put_item().table_name(self.entity_schema().table);
         let m = self.entity_to_av_map();
         for (k, v) in &m {
             req = req.item(k, v.clone());
@@ -98,3 +106,24 @@ pub trait DdbEntity {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::entity;
+
+    #[test]
+    fn av() {
+        let p = entity::Prediction {
+            prediction_id: "c".to_string(),
+            user_id: "d".to_string(),
+            condition: None,
+            created_at: None,
+        };
+
+        let avm = p.entity_to_av_map();
+
+        let pkv = avm.get("pk").unwrap().as_s().unwrap();
+        // println!("{}", pkv);
+        assert_eq!(pkv, "$Cdkrs#Prediction#predictionid_c");
+    }
+}
--- functions/hw/src/entity.rs
+++ functions/hw/src/entity.rs
@@ -1,57 +1,43 @@
 use crate::ddb::{Attribute, Composite, DdbEntity, EntitySchema, Index, IndexName, Key};
-use std::collections::HashMap;
+use maplit::hashmap;
 
 pub struct Prediction {
     pub prediction_id: String,
     pub user_id: String,
-    pub condition: String,
-    pub created_at: String,
+    pub condition: Option<String>,
+    pub created_at: Option<String>,
 }
 
 // todo: https://users.rust-lang.org/t/access-struct-attributes-by-string/17520/2
 impl DdbEntity for Prediction {
     fn entity_schema(&self) -> EntitySchema {
-        let mut indices = HashMap::new();
-        indices.insert(
-            IndexName::Primary,
-            Index {
-                partition_key: Key {
-                    field: "pk".to_string(),
-                    composite: vec![Composite {
-                        name: "prediction_id".to_string(),
-                        value: self.prediction_id.to_string(),
-                    }],
-                },
-                sort_key: Key {
-                    field: "sk".to_string(),
-                    composite: vec![],
-                },
-            },
-        );
-
-        let mut attributes = HashMap::new();
-        attributes.insert(
-            "prediction_id".to_string(),
-            Attribute::DdbString(self.prediction_id.to_string()),
-        );
-        attributes.insert(
-            "user_id".to_string(),
-            Attribute::DdbString(self.user_id.to_string()),
-        );
-        attributes.insert(
-            "condition".to_string(),
-            Attribute::DdbString(self.condition.to_string()),
-        );
-        attributes.insert(
-            "created_at".to_string(),
-            Attribute::DdbString(self.created_at.to_string()), // todo: time type
-        );
-
         EntitySchema {
+            table: "cdkrs-table".to_string(), // todo: read from env
             service: "Cdkrs".to_string(),
             entity: "Prediction".to_string(),
-            indices, // todo: hashlit?
-            attributes,
+            indices: hashmap! {
+                IndexName::Primary => {
+                    Index {
+                        partition_key: Key {
+                            field: "pk".to_string(),
+                            composite: vec![Composite {
+                                name: "predictionid".to_string(),
+                                value: self.prediction_id.to_string(),
+                            }],
+                        },
+                        sort_key: Key {
+                            field: "sk".to_string(),
+                            composite: vec![],
+                        },
+                    }
+                }
+            },
+            attributes: hashmap! {
+                "predictionid".to_string() => Attribute::DdbString(Some(self.prediction_id.clone())),
+                "userid".to_string() => Attribute::DdbString(Some(self.user_id.clone())),
+                "condition".to_string() => Attribute::DdbString(self.condition.clone()),
+                "createdat".to_string() => Attribute::DdbString(self.created_at.clone()),
+            },
         }
     }
 }
